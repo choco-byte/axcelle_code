69c9631759e3ca48508960e9d239b35b534b59ff
Stephanie <stephanie@students.mikroskil.ac.id>
2025-10-09 19:30:40 +0700

    Tickets page

--- lib/tickets_all.dart
+++ lib/tickets_all.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 class TicketsPage extends StatelessWidget {
+  const TicketsPage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Tickets Page',
-        style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+    return Scaffold(
+      body: const Center(
+        child: Text(
+          'Tickets Page',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
